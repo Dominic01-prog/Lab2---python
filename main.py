64439a732f8d76b48269d97035bf19c7b1eec9fa
Dominic01prog <djs7129@psu.edu>
2020-09-10 18:10:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Author: Dominic Savaglio
+# Collaborator: 
+# Collaborator:
+# Collaborator:
+# Section: 10R
+# Breakout: 
+def getLetterGrade(grade):
+  return "A"
+def run():
+  grade = input("Enter your CMPSC 131 grade: ")
+  print(getLetterGrade())
+if __name__ == "__main__":
+  run()
