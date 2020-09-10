5bac6a812d8a3267aeb6f8a66cddc10d30f7317e
Dominic01prog <djs7129@psu.edu>
2020-09-10 20:28:24 +0000

    Added if__name__ code

--- main.py
+++ main.py
@@ -5,9 +5,27 @@
 # Section: 10R
 # Breakout: 
 def getLetterGrade(grade):
-  return "A"
+  if grade >= 93.0:
+    return "A"
+  elif grade >= 90.0:
+    return "A-"
+  elif grade >= 87.0:
+    return "B+"
+  elif grade >= 83.0:
+    return "B"
+  elif grade >= 80.0:
+    return "B-"
+  elif grade >= 77.0:
+    return "C+"
+  elif grade >= 70.0:
+    return "C"
+  elif grade >= 60.0:
+    return "D"
+  else:
+    return "F"
 def run():
-  grade = input("Enter your CMPSC 131 grade: ")
-  print(getLetterGrade())
+  grade = float(input("Enter your CMPSC 131 grade: "))
+  letter_grade = getLetterGrade(grade)
+  print("Your letter grade for CMPSC 131 is " + letter_grade + ".")
 if __name__ == "__main__":
   run()
